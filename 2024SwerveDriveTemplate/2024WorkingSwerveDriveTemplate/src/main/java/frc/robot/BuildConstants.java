927aef59969ccc9ba65ab53e5a315d67a13e2113
AdamCodes2023 <adam.miller@reeths-puffer.org>
2024-01-13 12:07:43 -0500

    Working 2024 Swerve Drive Template

---
+++ 2024SwerveDriveTemplate/2024WorkingSwerveDriveTemplate/src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "3061-lib-main-Imported";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 2;
+  public static final String GIT_SHA = "7fae56cf52e94d9806ac81d54c94d047e56e0e01";
+  public static final String GIT_DATE = "2024-01-13 09:07:41 CST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-01-13 10:54:22 CST";
+  public static final long BUILD_UNIX_TIME = 1705164862189L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
